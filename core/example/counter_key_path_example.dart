9b5cdc3dd3fe1bac7ab452bdd97c1f31d4d3929e
Vinicius Rodrigues <vinicius.a.ro@gmail.com>
2025-01-30 17:42:11 -0200

    wip: readme-v0 showing basic usage with domain and reducer (#5)

---
+++ core/example/counter_key_path_example.dart
@@ -0,0 +1,48 @@
+import 'package:composable_architecture/core.dart';
+import 'package:http/http.dart' as http;
+
+@KeyPathable()
+final class FeatureState {
+  int count = 0;
+  String? numberFact;
+}
+
+@CaseKeyPathable()
+final class FeatureAction<
+    DecrementButtonTapped,
+    IncrementButtonTapped,
+    NumberFactButtonTapped,
+    NumberFactResponse extends NumberFactResponseValue //
+    > {}
+
+final class NumberFactResponseValue {
+  final String value;
+  NumberFactResponseValue(this.value);
+}
+
+Effect<FeatureAction> featureReducer(Inout<FeatureState> state, FeatureAction action) {
+  switch (action) {
+    case FeatureActionDecrementButtonTapped():
+      state.mutate((s) => s..count -= 1);
+      return Effect.none();
+
+    case FeatureActionIncrementButtonTapped():
+      state.mutate((s) => s..count += 1);
+      return Effect.none();
+
+    case FeatureActionNumberFactButtonTapped():
+      return Effect.future(() {
+        final uri = Uri.parse("http://numbersapi.com/${state.value.count})/trivia");
+        return http.post(uri).then(
+              (response) => FeatureAction.numberFactResponse(
+                NumberFactResponseValue(response.body),
+              ),
+            );
+      });
+
+    case FeatureActionNumberFactResponse():
+      state.mutate((s) => s..numberFact = action.numberFactResponse.value);
+      return Effect.none();
+  }
+  throw Exception("invalid action");
+}
