3ea2430f6daef2cd123cfbb65efef320705cbea2
Vinicius Rodrigues <vinicius.a.ro@gmail.com>
2025-05-01 17:22:27 -0300

    chore: unit tests and comments - core (#44)

--- tca_core/test/reducer_test.dart
+++ tca_core/test/reducer_test.dart
@@ -3,66 +3,59 @@ import 'dart:async';
 import 'package:composable_architecture/composable_architecture.dart';
 import 'package:test/test.dart';
 
-import '_helper.dart';
+import 'unit.dart';
 
 void main() {
-  group('reducer', () {
-    test('debug reducer prints debug information', () {
-      var printedLines = <String>[];
+  test('debug reducer prints debug information', () {
+    var printedLines = <String>[];
 
-      final specification = ZoneSpecification(
-        print: (self, parent, zone, line) => printedLines.add(line),
-      );
+    final specification = ZoneSpecification(
+      print: (self, parent, zone, line) => printedLines.add(line),
+    );
 
-      runZoned(
-        () {
-          final store = Store(
-            initialState: AppState(),
-            reducer: Reduce(counterReducer).debug(),
-          );
-          store.send(AppAction.actionA);
-        },
-        zoneSpecification: specification,
-      );
+    Effect<Unit> reducer(Inout<int> state, Unit action) {
+      state.mutate((s) => s + 1);
+      return Effect.none();
+    }
 
-      expect(printedLines, [
-        "--------",
-        "received action: AppAction.actionA",
-        "state: AppState(1)",
-      ]);
-    });
+    runZoned(
+      () {
+        final store = Store(
+          initialState: 0,
+          reducer: Reduce(reducer).debug(),
+        );
+        store.send(unit);
+      },
+      zoneSpecification: specification,
+    );
 
-    test('combine runs every reducer in list', () {
-      Effect<AppAction> reducer1(Inout<AppState> state, AppAction action) {
-        switch (action) {
-          case AppAction.actionA:
-            state.mutate((s) => s.copyWith(count: s.count + 10));
-            return Effect.none();
-          case AppAction.actionB:
-            return Effect.none();
-        }
-      }
+    expect(printedLines, [
+      "--------",
+      "received action: unit",
+      "state: 1",
+    ]);
+  });
 
-      Effect<AppAction> reducer2(Inout<AppState> state, AppAction action) {
-        switch (action) {
-          case AppAction.actionA:
-            state.mutate((s) => s.copyWith(count: s.count + 1));
-            return Effect.none();
-          case AppAction.actionB:
-            return Effect.none();
-        }
-      }
+  test('combine runs every reducer in list', () {
+    Effect<Unit> reducer1(Inout<int> state, Unit action) {
+      state.mutate((s) => s + 10);
+      return Effect.none();
+    }
 
-      final store = Store(
-        initialState: AppState(),
-        reducer: Reduce.combine([
-          Reduce(reducer1),
-          Reduce(reducer2),
-        ]),
-      );
+    Effect<Unit> reducer2(Inout<int> state, Unit action) {
+      state.mutate((s) => s + 1);
+      return Effect.none();
+    }
 
-      store.send(AppAction.actionA);
-      expect(store.state.count, 11);
-    });
+    final store = Store(
+      initialState: 0,
+      reducer: Reduce.combine([
+        Reduce(reducer1),
+        Reduce(reducer2),
+      ]),
+    );
+
+    store.send(unit);
+    expect(store.state, 11);
   });
 }
